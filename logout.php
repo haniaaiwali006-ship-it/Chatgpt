67e968c5c768e845926bd16aebe731168b5c5c1a
Hania AiWali <haniaaiwali006@gmail.com>
2026-03-05 12:17:03 +0500

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'database.php';
+
+// Destroy all session data
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to login page
+header("Location: login.php");
+exit();
+?>
